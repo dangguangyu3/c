b6736e44a75f6fdc1a3340df278abe1f638324c0
dgy <3423459998@qq.com>
2026-01-25 20:55:40 +0800

    first

---
+++ 22-array/cmake-build-debug/CMakeFiles/22_array.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "22_array.exe"
+  "22_array.exe.manifest"
+  "22_array.pdb"
+  "CMakeFiles/22_array.dir/main.c.obj"
+  "lib22_array.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/22_array.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 23-Access-out-of-bounds/cmake-build-debug/CMakeFiles/23_Access_out_of_bounds.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "23_Access_out_of_bounds.exe"
+  "23_Access_out_of_bounds.exe.manifest"
+  "23_Access_out_of_bounds.pdb"
+  "CMakeFiles/23_Access_out_of_bounds.dir/main.c.obj"
+  "lib23_Access_out_of_bounds.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/23_Access_out_of_bounds.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
